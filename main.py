2cf613868295b4cf4cdbd2b3ee02bf8d2f5feafe
liuchengyu0 <ban930318@gmail.com>
2025-05-15 14:09:39 +0800

    update: 修改 main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ async def predict(
     logger.info(f"API KEY: {x_api_key}")
     prediction = model.predict(features)
     #prediction = model.predict_proba(features)[:, 1]  # 取正類別 (1) 的機率值
-    #prediction = model.predict(features)[:, 1]  # 取正類別 (1) 的機率值
+
     print(model.predict(features))
     print(f"回傳數據: {prediction[0]}")
     # 轉換預測結果 (假設模型輸出的是 0~1 之間的機率，轉為百分比)
